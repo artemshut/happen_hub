4dfd79d2d9dc59754f711d025330d7b0f76b3edc
Artem Shut <artemshut@artems-macbook-air.home>
2025-08-20 10:59:13 +0200

    Update devise config

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -72,6 +72,7 @@ Devise.setup do |config|
   # modifying a user and when used to authenticate or find a user. Default is :email.
   config.strip_whitespace_keys = [ :email ]
 
+  config.mailer = "CustomDeviseMailer"
   # Tell if authentication through request.params is enabled. True by default.
   # It can be set to an array that will enable params authentication only for the
   # given strategies, for example, `config.params_authenticatable = [:database]` will
