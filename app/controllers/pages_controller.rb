3ce11c0d75d8c5f1db26035cc565898e853daf4a
Artem Shut <artemshut@artems-macbook-air.home>
2025-09-11 10:48:16 +0200

    remove code not working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,4 @@
 class PagesController < ActionController::Base
-  skip_before_action :authenticate_user!
-  
   def accept_cookies
     cookies[:consent_given] = { value: "1", expires: 1.year.from_now }
     redirect_back fallback_location: root_path
