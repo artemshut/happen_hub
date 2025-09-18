abae85b550e7e2d3412d57c6bb10a99915df2572
Artem Shut <artemshut@artems-macbook-air.home>
2025-09-18 16:35:02 +0200

    update serializers

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,6 +1,8 @@
 class EventSerializer
   include JSONAPI::Serializer
 
+  set_type :event
+  
   attributes :id, :title, :description, :start_time, :end_time, :slug
 
   belongs_to :user, serializer: UserSerializer
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,7 @@
 class UserSerializer
   include JSONAPI::Serializer
   
+  set_type :user
+
   attributes :id, :email, :username, :first_name, :last_name, :tag, :birthday
 end
