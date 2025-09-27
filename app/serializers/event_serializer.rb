80e30b7faa82c82753642548818bcab1ecd47d53
Artem Shut <artemshut@artems-macbook-air.home>
2025-09-27 12:00:10 +0200

    add cover image to events serializer

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -12,4 +12,12 @@ class EventSerializer
     event.users
   end
   has_many :event_participations, serializer: EventParticipationSerializer
+
+  attribute :cover_image_url do |event|
+    if event.cover_image.attached?
+      Rails.application.routes.url_helpers.rails_blob_url(
+        event.cover_image, only_path: false, host: 'happenhub.co'
+      )
+    end
+  end
 end
