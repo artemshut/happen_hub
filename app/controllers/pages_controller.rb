ebc28708301e4396a2ac7e2b5c074ec6dae60970
Artem Shut <artemshut@artems-macbook-air.home>
2025-11-21 14:51:07 +0100

    update terms

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ActionController::Base
+  layout "application"
+
   def accept_cookies
     cookies[:consent_given] = { value: "1", expires: 1.year.from_now }
     redirect_back fallback_location: root_path
@@ -9,6 +11,5 @@ class PagesController < ActionController::Base
 
   def terms_of_service
     @title = "Terms of Service"
-    render layout: "static"
   end
 end
