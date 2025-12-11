31a9ad3e312378b85363945ac50f288c597eb35f
Artem Shut <artemshut@Artems-MacBook-Air.local>
2025-12-11 17:13:03 +0100

    fix mailgun

--- config/initializers/mailgun.rb
+++ config/initializers/mailgun.rb
@@ -1,8 +1,11 @@
 require "mailgun-ruby"
 
-mailgun_api_key = Rails.application.credentials.dig(:mailgun, :sending_key)
+mailgun_credentials = Rails.application.credentials[:mailgun] || {}
+mailgun_api_key = mailgun_credentials[:sending_key]
+mailgun_host = mailgun_credentials[:api_host]
+
 MailgunClient = if mailgun_api_key.present?
-  Mailgun::Client.new(mailgun_api_key)
+  Mailgun::Client.new(mailgun_api_key, mailgun_host)
 else
   nil
 end
